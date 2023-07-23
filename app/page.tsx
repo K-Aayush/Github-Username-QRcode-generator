88fd005669f6edbdb2123178ed7bec9566a0e289
Aayush <ayushkarkee6@gmail.com>
2023-07-23 20:49:05 +0545

    fixed download

--- app/page.tsx
+++ app/page.tsx
@@ -23,9 +23,7 @@ export default function Home() {
     const link = document.createElement('a');
     link.href = src;
     link.download = 'qr-code.png';
-    document.body.appendChild(link);
     link.click();
-    document.body.removeChild(link);
   }
 
   return (
